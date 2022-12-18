c626010cfc2ab201697517256a3f397f86ff0fcb
Diego Yamasaki <yamasaki.diego@gmail.com>
2022-12-18 12:23:17 -0300

    aula 30 refatorando db_session

--- 05sqla_async/conf/db_session.py
+++ 05sqla_async/conf/db_session.py
@@ -1,57 +1,60 @@
-import sqlalchemy as sa
-
-from sqlalchemy.orm import sessionmaker
-
-from pathlib import Path # Usado no SQLite
+from pathlib import Path
 from typing import Optional
 
-from sqlalchemy.orm import Session
-from sqlalchemy.future.engine import Engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlalchemy.ext.asyncio import create_async_engine
 
 from models.model_base import ModelBase
 
+__async_engine: Optional[AsyncEngine] = None
 
-__engine: Optional[Engine] = None
+
+def create_engine(sqlite: bool = False) -> AsyncEngine:
+   global __async_engine
+   
+   if __async_engine:
+    return
+
+   if sqlite:
+    arquivo_db = 'db/picoles.sqlite'
+    folter = Path(arquivo_db).parent
+    folter.mkdir(parents=True, exist_ok=True)
+
+    conn_str = f'sqlite+aiosqlite:///{arquivo_db}'
+
+    __async_engine = create_async_engine(url=conn_str, echo=False, connect_args={"check_same_thread": False}) 
+   else:
+    # sem sqlite
+    conn_str = f'postgres+asyncpg://root:root@localhost:5432/picoles'
+    __async_engine = create_async_engine(url=conn_str, echo=False) 
 
 
-def create_engine(sqlite: bool = False):
-    global __engine
 
-    if __engine:
-        return __engine
+def create_session() -> AsyncSession:
+    global __async_engine
+
+    if not __async_engine:
+        create_async_engine()
     
-    if sqlite:
-        arquivo_db = 'db/picoles.sqlite'
-        folder = Path(arquivo_db).parent
-        folder.mkdir(parents=True, exist_ok=True)
+    __async_session = sessionmaker(
+        __async_engine,
+        expire_on_commit=False,
+        class_=AsyncSession
+    )
 
-        conn_str = f'sqlite:///{arquivo_db}'
-        __engine = sa.create_engine(url=conn_str, echo=False, connect_args={"check_same_thread": False})
-    else:
-        conn_str = f'mysql://root:root@localhost:3306/picoles'
-        __engine = sa.create_engine(url=conn_str, echo=False)
-
-
-def create_session() -> Session:
-    global __engine
-
-    if not __engine:
-        create_engine(True) # Para criar o banco como sqlite
-    
-    __session = sessionmaker(__engine, expire_on_commit=False, class_=Session)
-
-    session: Session = __session()
-    session.execute("PRAGMA foreign_keys = ON") # Usando somente para sqlite pois ele desabilita chave estrangeira.
+    session: AsyncSession = __async_session()
 
     return session
 
+async def create_tables() -> None:
+    global __async_engine
 
-def create_tables() -> None:
-    global __engine    
-
-    if not __engine:
-        create_engine(True)
+    if not __async_engine:
+        create_async_engine()
     
     import models.__all__models
-    ModelBase.metadata.drop_all(__engine)
-    ModelBase.metadata.create_all(__engine)
+    async with __async_engine.begin() as conn:
+        await conn.run_sync(ModelBase.metadata.drop_all)
+        await conn.runc_async(ModelBase.metadata.create_all)
